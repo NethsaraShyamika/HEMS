6bb5cd4b9c2424acaed2ba31614c921ad5fc821c
Nethsara Shyamika <nethsarashyamika@gmail.com>
2025-08-31 12:13:26 +0530

    Create MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 app.use(cors())
@@ -8,3 +9,14 @@ app.use(express.json())
 app.listen(process.env.PORT, () => {
     console.log(`Server is Running on port ${process.env.PORT}`)
 })
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+connectDB();
