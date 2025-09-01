4762cbbba0f93dcd85d21dd5c305b10a37a07888
Sandeepa Gunathilaka <dsandeepa.gunathilaka@gmail.com>
2025-09-01 15:51:55 +0530

    Made changes to header and layout

--- frontend/src/Components/Layout/DashboardLayout.jsx
+++ frontend/src/Components/Layout/DashboardLayout.jsx
@@ -4,10 +4,7 @@ import Header from "./Header";
 function DashboardLayout({ children }) {
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Main content */}
+      
       <div className="flex flex-col flex-1">
         <Header />
         <main className="p-6 overflow-y-auto">{children}</main>
