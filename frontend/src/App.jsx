50419cc699572443b65482425f534cbd761a6410
Sandeepa Gunathilaka <dsandeepa.gunathilaka@gmail.com>
2025-09-01 13:02:52 +0530

    Added Header and HR dashboard jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HRDashboard from "./pages/HRDashboard";
+import "./App.css";
 
 function App() {
   return (
-    <div className='text-teal-500'>welcome to ems</div>
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        {/* Reusable Navbar */}
+        <Header />
+
+        {/* Page Content */}
+        <main className="flex-grow p-4 bg-gray-100">
+          <Routes>
+            <Route path="/" element={<HRDashboard />} />
+            <Route path="/hr-dashboard" element={<HRDashboard />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
